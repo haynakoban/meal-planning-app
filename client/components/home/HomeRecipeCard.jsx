5c1bd8c6a99560fc3871ffdede7638efce7452f5
Bryan Cortez <bancortez2@gmail.com>
2023-08-31 01:06:26 +0800

    fixed Home Recipes component and style

---
+++ client/components/home/HomeRecipeCard.jsx
@@ -0,0 +1,70 @@
+import { Text, View } from 'react-native';
+import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+
+import FavoriteCard from '../../components/favorites/FavoriteCard';
+import { COLORS, SIZES } from '../../constants';
+import styles from '../../styles/homeRecipes';
+
+import { data } from '../../screens/favorites/FavoritesScreen';
+
+const HomeRecipeCard = ({ headerTitle, subTitle }) => {
+  const navigation = useNavigation();
+
+  const {
+    recipeContainer,
+    headerContainer,
+    headerTitleStyle,
+    headerButtonContent,
+    headerButtonStyle,
+    headerButtonLabel,
+    cardWrapper,
+    cardContentWrapper,
+  } = styles;
+
+  return (
+    <View style={recipeContainer}>
+      <View style={headerContainer}>
+        <Text style={headerTitleStyle}>{headerTitle}</Text>
+        <Button
+          mode='text'
+          icon={() => (
+            <FontAwesome
+              name='angle-right'
+              size={SIZES.lg}
+              color={COLORS.black}
+            />
+          )}
+          contentStyle={headerButtonContent}
+          style={headerButtonStyle}
+          textColor={COLORS.black}
+          labelStyle={headerButtonLabel}
+          onPress={() => {
+            navigation.navigate('Show All Recipes', {
+              title: subTitle,
+            });
+          }}
+        >
+          See all
+        </Button>
+      </View>
+
+      <View style={cardWrapper}>
+        {data.map(({ id, name, username, ratings, image }) => (
+          <View key={id} style={cardContentWrapper}>
+            <FavoriteCard
+              name={name}
+              username={username}
+              ratings={ratings}
+              image={image}
+              key={id}
+            />
+          </View>
+        ))}
+      </View>
+    </View>
+  );
+};
+
+export default HomeRecipeCard;
--- client/screens/home/HomeScreen.jsx
+++ client/screens/home/HomeScreen.jsx
@@ -1,295 +1,44 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { ScrollView } from 'react-native';
 
-import { data } from '../favorites/FavoritesScreen';
-import FavoriteCard from '../../components/favorites/FavoriteCard';
-import { Button } from 'react-native-paper';
-import { COLORS, FONT, SIZES } from '../../constants';
+import HomeRecipeCard from '../../components/home/HomeRecipeCard';
 
+const data = [
+  {
+    id: 1,
+    headerTitle: 'Recommended Recipes',
+    subTitle: 'Recommended',
+  },
+  {
+    id: 2,
+    headerTitle: 'Breakfast',
+    subTitle: 'Breakfast',
+  },
+  {
+    id: 3,
+    headerTitle: 'Lunch',
+    subTitle: 'Lunch',
+  },
+  {
+    id: 4,
+    headerTitle: 'Dinner',
+    subTitle: 'Dinner',
+  },
+];
 const HomeScreen = () => {
-  const navigation = useNavigation();
-
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={{ marginBottom: 16 }}>
-        <View
-          style={{
-            paddingHorizontal: 16,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Text style={{ fontFamily: FONT.semiBold, fontSize: SIZES.md }}>
-            Recommended Recipes
-          </Text>
-          <Button
-            mode='text'
-            icon={() => (
-              <FontAwesome
-                name='angle-right'
-                size={SIZES.lg}
-                color={COLORS.black}
-              />
-            )}
-            contentStyle={{
-              flexDirection: 'row-reverse',
-              justifyContent: 'flex-start',
-            }}
-            style={{ alignItems: 'center' }}
-            textColor={COLORS.black}
-            labelStyle={{
-              fontFamily: FONT.semiBold,
-              fontSize: SIZES.sm,
-              paddingTop: 1,
-            }}
-            onPress={() => {
-              navigation.navigate('Show All Recipes', {
-                title: 'Recommended',
-              });
-            }}
-          >
-            See all
-          </Button>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            paddingHorizontal: 4,
-          }}
-        >
-          {data.map((item) => (
-            <View
-              key={item.id}
-              style={{
-                width: '50%',
-              }}
-            >
-              <FavoriteCard
-                name={item.name}
-                username={item.username}
-                ratings={item.ratings}
-                image={item.image}
-                key={item.id}
-              />
-            </View>
-          ))}
-        </View>
-      </View>
-      <View style={{ marginBottom: 16 }}>
-        <View
-          style={{
-            paddingHorizontal: 16,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Text style={{ fontFamily: FONT.semiBold, fontSize: SIZES.md }}>
-            Breakfast
-          </Text>
-          <Button
-            mode='text'
-            icon={() => (
-              <FontAwesome
-                name='angle-right'
-                size={SIZES.lg}
-                color={COLORS.black}
-              />
-            )}
-            contentStyle={{
-              flexDirection: 'row-reverse',
-              justifyContent: 'flex-start',
-            }}
-            style={{ alignItems: 'center' }}
-            textColor={COLORS.black}
-            labelStyle={{
-              fontFamily: FONT.semiBold,
-              fontSize: SIZES.sm,
-              paddingTop: 1,
-            }}
-            onPress={() => {
-              navigation.navigate('Show All Recipes', {
-                title: 'Breakfast',
-              });
-            }}
-          >
-            See all
-          </Button>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            paddingHorizontal: 4,
-          }}
-        >
-          {data.map((item) => (
-            <View
-              key={item.id}
-              style={{
-                width: '50%',
-              }}
-            >
-              <FavoriteCard
-                name={item.name}
-                username={item.username}
-                ratings={item.ratings}
-                image={item.image}
-                key={item.id}
-              />
-            </View>
-          ))}
-        </View>
-      </View>
-      <View style={{ marginBottom: 16 }}>
-        <View
-          style={{
-            paddingHorizontal: 16,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Text style={{ fontFamily: FONT.semiBold, fontSize: SIZES.md }}>
-            Lunch
-          </Text>
-          <Button
-            mode='text'
-            icon={() => (
-              <FontAwesome
-                name='angle-right'
-                size={SIZES.lg}
-                color={COLORS.black}
-              />
-            )}
-            contentStyle={{
-              flexDirection: 'row-reverse',
-              justifyContent: 'flex-start',
-            }}
-            style={{ alignItems: 'center' }}
-            textColor={COLORS.black}
-            labelStyle={{
-              fontFamily: FONT.semiBold,
-              fontSize: SIZES.sm,
-              paddingTop: 1,
-            }}
-            onPress={() => {
-              navigation.navigate('Show All Recipes', {
-                title: 'Lunch',
-              });
-            }}
-          >
-            See all
-          </Button>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            paddingHorizontal: 4,
-          }}
-        >
-          {data.map((item) => (
-            <View
-              key={item.id}
-              style={{
-                width: '50%',
-              }}
-            >
-              <FavoriteCard
-                name={item.name}
-                username={item.username}
-                ratings={item.ratings}
-                image={item.image}
-                key={item.id}
-              />
-            </View>
-          ))}
-        </View>
-      </View>
-      <View style={{ marginBottom: 16 }}>
-        <View
-          style={{
-            paddingHorizontal: 16,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Text style={{ fontFamily: FONT.semiBold, fontSize: SIZES.md }}>
-            Dinner
-          </Text>
-          <Button
-            mode='text'
-            icon={() => (
-              <FontAwesome
-                name='angle-right'
-                size={SIZES.lg}
-                color={COLORS.black}
-              />
-            )}
-            contentStyle={{
-              flexDirection: 'row-reverse',
-              justifyContent: 'flex-start',
-            }}
-            style={{ alignItems: 'center' }}
-            textColor={COLORS.black}
-            labelStyle={{
-              fontFamily: FONT.semiBold,
-              fontSize: SIZES.sm,
-              paddingTop: 1,
-            }}
-            onPress={() => {
-              navigation.navigate('Show All Recipes', {
-                title: 'Dinner',
-              });
-            }}
-          >
-            See all
-          </Button>
-        </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            paddingHorizontal: 4,
-          }}
-        >
-          {data.map((item) => (
-            <View
-              key={item.id}
-              style={{
-                width: '50%',
-              }}
-            >
-              <FavoriteCard
-                name={item.name}
-                username={item.username}
-                ratings={item.ratings}
-                image={item.image}
-                key={item.id}
-              />
-            </View>
-          ))}
-        </View>
-      </View>
+      {data.map(({ id, headerTitle, subTitle }) => {
+        return (
+          <HomeRecipeCard
+            key={id}
+            headerTitle={headerTitle}
+            subTitle={subTitle}
+          />
+        );
+      })}
     </ScrollView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 8,
-    elevation: 5,
-  },
-});
 
 export default HomeScreen;
