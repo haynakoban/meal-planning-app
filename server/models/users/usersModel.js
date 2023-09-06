d4bda9cc14c63706b28d94e13fb973a7f9f83002
Bryan Cortez <bancortez2@gmail.com>
2023-09-07 04:31:54 +0800

    users

--- server/models/users/usersModel.js
+++ server/models/users/usersModel.js
@@ -1,5 +1,23 @@
 const mongoose = require('mongoose');
 
+const public_metrics = {
+  followers_count: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  following_count: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  following: {
+    type: Array,
+    ref: 'Users',
+    required: false,
+  },
+};
+
 const usersSchema = new mongoose.Schema({
   fullname: {
     type: String,
@@ -10,6 +28,10 @@ const usersSchema = new mongoose.Schema({
     required: true,
     unique: true,
     lowercase: true,
+  },
+  bio: {
+    type: String,
+    required: false,
   },
   password: {
     type: String,
@@ -26,6 +48,10 @@ const usersSchema = new mongoose.Schema({
   favorites: {
     type: Array,
     required: false,
+  },
+  public_metrics: {
+    type: Object,
+    public_metrics,
   },
   createdAt: {
     type: Date,
