3b7ea7e17c74705bd60ad6e7a29a200ad4abe9fa
Bryan Cortez <bancortez2@gmail.com>
2023-09-13 19:24:59 +0800

    recipe model

--- server/models/recipes/recipesModel.js
+++ server/models/recipes/recipesModel.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-const ingredientsObj = {
+const ingredients = {
+  type: Array,
+  ref: 'Ingredients',
+  required: true,
   ingredients_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Ingredients',
@@ -11,51 +14,29 @@ const ingredientsObj = {
   description: { type: String, required: true },
 };
 
+const user_id = {
+  type: mongoose.Schema.Types.ObjectId,
+  ref: 'Users',
+  required: true,
+};
+
+const image = {
+  type: mongoose.Schema.Types.Mixed,
+  ref: 'Uploads',
+  required: false,
+};
+
 const recipesSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Users',
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  procedure: {
-    type: Array,
-    required: true,
-  },
-  type: {
-    type: Array,
-    ref: 'Meal_Types',
-    required: true,
-  },
-  ingredients: {
-    type: Array,
-    ref: 'Ingredients',
-    ingredientsObj,
-    required: true,
-  },
-  preferences: {
-    type: Array,
-    ref: 'Preferences',
-    required: false,
-  },
-  cuisines: {
-    type: Array,
-    ref: 'Cuisines',
-    required: false,
-  },
+  user_id,
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  procedure: { type: Array, required: true },
+  meal_types: { type: Array, ref: 'Meal_Types', required: true },
+  preferences: { type: Array, ref: 'Preferences', required: false },
+  cuisines: { type: Array, ref: 'Cuisines', required: false },
   cooking_time: { type: Number, required: false },
-  image: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Uploads',
-    required: false,
-  },
+  ingredients,
+  image,
   privacy: { type: String, required: true },
   createdAt: { type: Date, default: () => Date.now(), immutable: true },
   updatedAt: { type: Date, default: () => Date.now() },
