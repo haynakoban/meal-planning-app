2dc54dfc95f7ef730d9d090135fc9bcb97a4ee80
Bryan Cortez <bancortez2@gmail.com>
2023-09-14 09:53:14 +0800

    recipes model

--- server/controllers/recipes/recipesController.js
+++ server/controllers/recipes/recipesController.js
@@ -65,8 +65,76 @@ const list = async (req, res, next) => {
   }
 };
 
+// get the paginated list of recipes
+const paginatedList = async (req, res, next) => {
+  const page = parseInt(req.query.page) || 1;
+  const perPage = 20;
+
+  try {
+    // Query the database for recipes, skipping the appropriate number of documents based on the page
+    const recipes = await Recipes.find()
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .select('-createAt -__v');
+
+    // Get the total count of recipes in the collection (for calculating total pages)
+    const totalItems = await Recipes.countDocuments();
+
+    // Calculate the total number of pages based on the total count and items per page
+    const totalPages = Math.ceil(totalItems / perPage);
+
+    // Return the paginated data along with pagination information
+    res.json({
+      message: `${recipes.length} items retrieved successfully`,
+      status: 'success',
+      currentPage: page,
+      totalPages,
+      data: recipes,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Internal Server Error',
+      status: 'error occurred',
+      data: [],
+    });
+  }
+};
+
+// get single recipe
+const show = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    // Query the database to find the recipe by its unique ID
+    const recipe = await Recipes.findOne({ _id: id });
+
+    if (!recipe) {
+      return res.status(404).json({
+        message: 'Item not found',
+        status: 'error occurred',
+        data: {},
+      });
+    }
+
+    // Return the recipe data
+    res.json({
+      message: 'Item retrieved successfully',
+      status: 'success',
+      data: recipe,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Internal Server Error',
+      status: 'error occurred',
+      data: [],
+    });
+  }
+};
+
 module.exports = {
   bulkRecipes,
   create,
   list,
+  paginatedList,
+  show,
 };
--- server/routes/recipesRoute.js
+++ server/routes/recipesRoute.js
@@ -16,6 +16,10 @@ router
   .route('/bulk')
   .post(checkBulkUniquenessMiddleware(Recipes), recipesController.bulkRecipes);
 
-module.exports = router;
+// get method - get the paginated list of recipes
+router.route('/list').get(recipesController.paginatedList);
+
+// get method - get single recipe
+router.route('/:id').get(recipesController.show);
 
 module.exports = router;
