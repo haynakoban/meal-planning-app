3bdbadf27378d9b223176b86bcfd2700d2f1e1b5
Xenon <x.vergara2201@gmail.com>
2023-10-02 15:32:42 +0800

    filter recipes

--- client/components/general/FilterDropdown.jsx
+++ client/components/general/FilterDropdown.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   ScrollView,
   SectionList,
@@ -14,6 +14,7 @@ import { COLORS, FONT, SIZES } from '../../constants';
 import styles from '../../styles/filterDropDown';
 import useFilterStore from '../../store/useFilterStore';
 import useRecipeStore from '../../store/useRecipeStore';
+import LoadingScreen from '../../screens/loading/LoadingScreen';
 
 const FilterDropdown = ({ hideModal }) => {
   const [openSections, setOpenSections] = useState(['Meal Types']);
@@ -27,6 +28,11 @@ const FilterDropdown = ({ hideModal }) => {
     dropdownButtonContent,
   } = styles;
 
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(false);
+  }, [filters]);
+
   // Function to toggle section open/close
   const toggleSection = (sectionTitle) => {
     if (openSections.includes(sectionTitle)) {
@@ -37,106 +43,124 @@ const FilterDropdown = ({ hideModal }) => {
   };
 
   return (
-    <ScrollView>
-      <View
-        style={{
-          paddingHorizontal: 16,
-          paddingBottom: 12,
-          flexDirection: 'row',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          borderColor: COLORS.lightBlack,
-          borderBottomWidth: 1,
-        }}
-      >
-        <TouchableOpacity onPress={hideModal}>
-          <Ionicons name='close' size={SIZES.xl + 4} color={COLORS.black} />
-        </TouchableOpacity>
-        <Text
-          style={{
-            fontFamily: FONT.medium,
-            fontSize: SIZES.md + 1,
-            marginHorizontal: 8,
-            flexGrow: 1,
-          }}
-        >
-          Filter Recipes
-        </Text>
-        <Button
-          mode='contained-tonal'
-          style={{
-            paddingHorizontal: 6,
-            borderRadius: 12,
-          }}
-          labelStyle={{
-            fontFamily: FONT.bold,
-            fontSize: SIZES.sm - 1,
-          }}
-          compact={true}
-          onPress={() => clearFilter()}
-        >
-          Clear All
-        </Button>
-      </View>
-      <SectionList
-        scrollEnabled={false}
-        sections={filters}
-        keyExtractor={(item) => item._id.toString()}
-        renderItem={({ item, section }) => {
-          if (openSections.includes(section.title)) {
-            return (
+    <>
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <ScrollView>
+          <View
+            style={{
+              paddingHorizontal: 16,
+              paddingBottom: 12,
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              alignItems: 'center',
+              borderColor: COLORS.lightBlack,
+              borderBottomWidth: 1,
+            }}
+          >
+            <TouchableOpacity onPress={hideModal}>
+              <Ionicons name='close' size={SIZES.xl + 4} color={COLORS.black} />
+            </TouchableOpacity>
+            <Text
+              style={{
+                fontFamily: FONT.medium,
+                fontSize: SIZES.md + 1,
+                marginHorizontal: 8,
+                flexGrow: 1,
+              }}
+            >
+              Filter Recipes
+            </Text>
+            <Button
+              mode='contained-tonal'
+              style={{
+                paddingHorizontal: 6,
+                borderRadius: 12,
+              }}
+              labelStyle={{
+                fontFamily: FONT.bold,
+                fontSize: SIZES.sm - 1,
+              }}
+              compact={true}
+              onPress={() => clearFilter()}
+            >
+              Clear All
+            </Button>
+          </View>
+          <SectionList
+            scrollEnabled={false}
+            sections={filters}
+            keyExtractor={(item) => item._id.toString()}
+            renderItem={({ item, section }) => {
+              if (openSections.includes(section.title)) {
+                return (
+                  <Button
+                    icon={() => {
+                      if (filteredData[section.title]?.includes(item._id)) {
+                        return (
+                          <Ionicons name='checkbox' size={24} color='black' />
+                        );
+                      } else {
+                        return (
+                          <MaterialCommunityIcons
+                            name='checkbox-blank-outline'
+                            size={24}
+                            color='black'
+                          />
+                        );
+                      }
+                    }}
+                    contentStyle={[buttonContent, pd]}
+                    labelStyle={[
+                      buttonLabel,
+                      {
+                        fontFamily: filteredData[section.title]?.includes(
+                          item._id
+                        )
+                          ? FONT.semiBold
+                          : FONT.medium,
+                      },
+                    ]}
+                    textColor={COLORS.black}
+                    onPress={() => setFilteredData(section.title, item._id)}
+                  >
+                    {item?.time || item.name}
+                  </Button>
+                );
+              } else {
+                return null;
+              }
+            }}
+            renderSectionHeader={({ section: { title } }) => (
               <Button
+                textColor={COLORS.black}
                 icon={() => {
-                  if (filteredData[section.title]?.includes(item._id)) {
-                    return <Ionicons name='checkbox' size={24} color='black' />;
+                  if (openSections.includes(title)) {
+                    return (
+                      <Ionicons name='chevron-down' size={24} color='black' />
+                    );
                   } else {
                     return (
-                      <MaterialCommunityIcons
-                        name='checkbox-blank-outline'
-                        size={24}
-                        color='black'
-                      />
+                      <Ionicons name='chevron-up' size={24} color='black' />
                     );
                   }
                 }}
-                contentStyle={[buttonContent, pd]}
-                labelStyle={[
-                  buttonLabel,
-                  {
-                    fontFamily: filteredData[section.title]?.includes(item._id)
-                      ? FONT.semiBold
-                      : FONT.medium,
-                  },
-                ]}
-                textColor={COLORS.black}
-                onPress={() => setFilteredData(section.title, item._id)}
+                labelStyle={dropdownButtonLabel}
+                contentStyle={dropdownButtonContent}
+                onPress={() => toggleSection(title)}
               >
-                {item?.time || item.name}
+                {title === 'CookingTimes'
+                  ? 'Cooking Times'
+                  : title === 'MealTypes'
+                  ? 'Meal Types'
+                  : title}
               </Button>
-            );
-          } else {
-            return null;
-          }
-        }}
-        renderSectionHeader={({ section: { title } }) => (
-          <Button
-            textColor={COLORS.black}
-            icon={() => {
-              if (openSections.includes(title)) {
-                return <Ionicons name='chevron-down' size={24} color='black' />;
-              } else {
-                return <Ionicons name='chevron-up' size={24} color='black' />;
-              }
-            }}
-            labelStyle={dropdownButtonLabel}
-            contentStyle={dropdownButtonContent}
-            onPress={() => toggleSection(title)}
-          >
-            {title}
-          </Button>
-        )}
-      />
-    </ScrollView>
+            )}
+          />
+        </ScrollView>
+      )}
+    </>
   );
 };
 
